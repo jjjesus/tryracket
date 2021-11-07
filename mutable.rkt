83f2212e4ac4a54d78d4afaeb60c78abf5f24f92
jjesus <john.j.jesus@gmail.com>
2021-11-07 15:58:57 -0600

    Looop without mutable

--- mutable.rkt
+++ mutable.rkt
@@ -7,3 +7,14 @@
 (count)    ; returns 1
 
 (count)    ; returns 2
+
+
+(define (count-whales lst)
+    (for/fold ([c 0])
+            ([thing lst])
+        (if (equal? thing "shamu")
+            (add1 c)
+            c)))
+
+;; Example:
+(count-whales '("shamu" "donald duck" "shamu"))
